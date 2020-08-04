ca2198fb82672895c8181cf7d02cc77d6280d2be
Cristian <crgaonas24@gmail.com>
2020-08-04 04:34:09 -0500

    Cambios en las graficas

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -11,6 +11,7 @@ import { AreaComponent } from './widgets/area/area.component';
 import { HighchartsChartModule } from 'highcharts-angular';
 import { CardComponent } from './widgets/card/card.component';
 import { PieComponent } from './widgets/pie/pie.component';
+import { Area1Component } from './widgets/area1/area1.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { PieComponent } from './widgets/pie/pie.component';
     SidebarComponent,
     AreaComponent,
     CardComponent,
-    PieComponent
+    PieComponent,
+    Area1Component
   ],
   imports: [
     CommonModule,
@@ -39,7 +41,8 @@ import { PieComponent } from './widgets/pie/pie.component';
     SidebarComponent,
     AreaComponent,
     CardComponent,
-    PieComponent
+    PieComponent,
+    Area1Component
   ]
 })
 export class SharedModule { }
---
+++ src/app/shared/widgets/area1/area1.component.ts
@@ -0,0 +1,93 @@
+import { Component, OnInit, Input } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
+import { DashboardService } from 'src/app/modules/dashboard.service';
+
+@Component({
+  selector: 'app-widget-area1',
+  templateUrl: './area1.component.html',
+  styleUrls: ['./area1.component.scss']
+})
+export class Area1Component implements OnInit {
+  chartOptions: {};
+  @Input() data: any = [];
+
+  users: any  =[]
+  Highcharts: typeof Highcharts = Highcharts;
+ // Highcharts = Highcharts;
+  graficaPrueba:Highcharts.Options ={
+    chart: {
+      type: 'area'
+    },
+    title: {
+      text: 'Casos Covid-19'
+    },
+    subtitle: {
+      text: 'Europa'
+    },
+    tooltip: {
+      split: true,
+      valueSuffix: ' cases'
+    },
+    credits: {
+      enabled: false
+    },
+    exporting: {
+      enabled: true,
+    },
+    xAxis:{
+      categories:[]
+    },
+    plotOptions: {
+      area: {
+          stacking: 'normal',
+          lineColor: '#666666',
+          lineWidth: 1,
+          marker: {
+              lineWidth: 1,
+              lineColor: '#666666'
+          }
+      }
+  },
+    series: [
+      {
+         name:'Europa Cases',
+         data: [],
+         type: 'area'
+      }
+      
+           ]
+  
+  };
+
+  constructor(protected service:DashboardService ) { }
+
+  ngOnInit() {
+    this.service.covid19Reports()
+    .subscribe(
+      (data) => { // Success
+        this.users = data;
+        const datosGrafica = this.users.map(x => x.cases);
+        const nombre = this.users.map(x  => x.country);
+
+        //Highcharts
+        this.graficaPrueba.series[0]['data'] = datosGrafica;
+        this.graficaPrueba.xAxis['categories'] = nombre;
+        Highcharts.chart('MediosdPPrincipal2', this.graficaPrueba);
+
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+HC_exporting(Highcharts);
+
+setTimeout(() => {
+  window.dispatchEvent(
+    new Event('resize')
+  );
+}, 300);
+    
+  }
+
+}
